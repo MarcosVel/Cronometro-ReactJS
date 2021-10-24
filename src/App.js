77d4c3413254ae18cf414c4cc5afe008aa393cbd
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-24 16:44:53 -0300

    created styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Cronometro from './assets/crono.png';
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img src={ Cronometro } className="img" />
+        <p className="timer">0.0</p>
+        <div className='areaBtn'>
+          <a className="btn">VAI</a>
+          <a className="btn">LIMPAR</a>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
