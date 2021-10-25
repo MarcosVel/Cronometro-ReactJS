5c58ebe8102e997c517d1644f5966e69b69d2658
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-24 23:42:32 -0300

    add function limpar

--- src/App.js
+++ src/App.js
@@ -36,7 +36,15 @@ class App extends Component {
   }
 
   limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
 
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'VAI';
+    this.setState(state);
   }
 
   render() {
