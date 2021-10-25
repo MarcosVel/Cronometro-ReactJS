652312e60a453f7d4e8ca6633a0c36b60559cc76
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-24 23:39:21 -0300

    add function vai

--- src/App.js
+++ src/App.js
@@ -3,14 +3,50 @@ import Cronometro from './assets/crono.png';
 import './styles.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'VAI',
+    }
+
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  vai() {
+    let state = this.state;
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'VAI';
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.botao = 'PAUSAR';
+    }
+
+    this.setState(state);
+  }
+
+  limpar() {
+
+  }
+
   render() {
     return (
       <div className="container">
         <img src={ Cronometro } className="img" />
-        <p className="timer">0.0</p>
+        <p className="timer">{ this.state.numero.toFixed(1) }</p>
         <div className='areaBtn'>
-          <a className="btn">VAI</a>
-          <a className="btn">LIMPAR</a>
+          <a className="btn" onClick={ this.vai }>{ this.state.botao }</a>
+          <a className="btn" onClick={ this.limpar }>LIMPAR</a>
         </div>
       </div>
     )
